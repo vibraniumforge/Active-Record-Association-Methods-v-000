5c524db2155ad4fa9b4d47493cde952e4d7c6a11
Greg Morgan <gregm9999@gmail.com>
2018-11-24 21:30:48 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,13 +11,13 @@ class Genre < ActiveRecord::Base
 
   def artist_count
     # return the number of artists associated with the genre
-    binding.pry
+    # binding.pry
     self.artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    binding.pry
+    # binding.pry
     x=[]
     x<<self.artists.name
     x
